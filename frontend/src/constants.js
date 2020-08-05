6eb7d674c596287903c0fad33de4d2ebef26087d
mightykim91 <mightykim91@naver.com>
2020-08-05 16:48:49 +0900

    Fix backend address
    Former-commit-id: f85421b652b00fc6f395e1d9b9617d44f4156c60

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 export default Object.freeze({
-    // URL: 'http://i3a503.p.ssafy.io:8080/'
+    URL: 'http://i3a503.p.ssafy.io:8080/'
     //local
-    URL: 'http://localhost:8080/'
+    // URL: 'http://localhost:8080/'
 })
 
