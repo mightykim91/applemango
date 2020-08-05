e46d59413ad3c08f1ae045d9f11039f45e02cf8f
박기훈 <gh9411@gmail.com>
2020-08-05 12:54:52 +0900

    favor path / v-list
    Former-commit-id: a4a908bc0af764e63c35585bb07ee82626b2bf9b

--- frontend/src/views/user/myPage.vue
+++ frontend/src/views/user/myPage.vue
@@ -8,21 +8,18 @@
         <h1>즐겨찾기 리스트</h1>
         
         <!--크롤링 데이터 리스트 컨테이너-->
-        <v-container fluid>
-            <v-row>
-                <v-card flat class="text-xs-center ma-3" v-for="favors in favorlist" v-bind:key="favors.rid">                    
-                    <button>{{favors}}</button><br>
-                    <button>{{favors.uid}}</button><br>
-                    <button>{{favors.rid}}</button>
-                </v-card>
-            </v-row>
-        </v-container>
+
 
         <v-list>
             <template  v-for="favors in favorlist">
-                <v-list-item :key="favors.fid">
-                    {{favors}}
+                <v-list-item :key="favors.fid" v-bind:to= "{ name: 'storeDetail', params: { rid: favors.frid }}">
+                    <v-list-item-action>
 
+                    </v-list-item-action>
+                    <v-list-item-content>
+                    <v-list-item-title v-text="favors.restaurant.rname"></v-list-item-title>
+                    <v-list-item-subtitle v-text="favors.restaurant.rbranch"></v-list-item-subtitle>
+                    </v-list-item-content>
                 </v-list-item>
             </template>
         </v-list>
@@ -45,11 +42,7 @@
                     id : '',
                     password : ''
                 },
-                favorlist : {
-                    fid : '',
-                    uid : '',
-                    rid : ''
-                }
+                favorlist : []
             }
 
         },
