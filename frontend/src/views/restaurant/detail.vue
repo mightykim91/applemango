ff52095a69a6114531b0a360f6a6d631bbb922d8
So Ra Chin <sorac725@gmail.com>
2020-08-05 09:50:10 +0900

    mod detail page
    Former-commit-id: 962f17635de4b11ab1d8fe36512784eee994e6f8

--- frontend/src/views/restaurant/detail.vue
+++ frontend/src/views/restaurant/detail.vue
@@ -54,20 +54,19 @@
                     <td class="text-center">{{menu.mid}}</td>
                     <td class="text-center">{{menu.mname}}</td>
                     <td class="text-center">{{menu.mprice}}원</td>
-                    <td class="text-center">{{menu.mimage}}</td>
+                    <td class="text-center"><img id = "menuimg" :src="menu.mimage"/></td>
                     <td>
-                        <b-button v-b-modal.modMenu>수정</b-button>
-                        <b-button v-b-modal.delMenu>삭제</b-button>       
+                        <b-link v-b-modal.modMenu>수정</b-link><br>
+                        <b-link v-b-modal.delMenu>삭제</b-link>       
                     </td>
                         <!-- 메뉴 수정하는 모달 창 -->
-                        <b-modal id="modMenu" ref="modal" 
-                            title="메뉴 수정" @ok="modhandleSubmit">
-                            <form ref="form">
-                                <b-form-group  
-                                    label-for="input" invalid-feedback="required">
+                        <b-modal id="modMenu" title="메뉴 수정" @ok="modhandleSubmit">
+                            <form>
+                                <b-form-group invalid-feedback="required">
+                                    {{menu.mname}}
                                     이름<b-form-input v-model="newname" :placeholder= "menu.mname" :value="menu.mname" requried></b-form-input>
                                     <b-form-checkbox v-model="newissig" requried>메인메뉴</b-form-checkbox> 
-                                    가격 <b-form-input v-model="newprice" :placeholder= "menu.mprice" required></b-form-input>
+                                    가격 <b-form-input v-model="newprice" :placeholder= "menu.mprice " required></b-form-input>
                                     <!-- 이미지 업로드 or 이미지 주소 복사(현재는 이미지 주소) -->
                                     이미지 <b-form-input v-model="newimage" :placeholder= "menu.mimage" required></b-form-input>
                                 </b-form-group>
@@ -174,7 +173,7 @@ export default {
         //메뉴삭제처리
         delhandleSubmit: function(mid) {
             console.log("삭제할 메뉴 번호:"+mid);
-            axios.post(BACKEND_URL + '/menu/del?mid=' + mid).then(response => {
+            axios.get(BACKEND_URL + '/menu/del?mid=' + mid).then(response => {
                 console.log(response.data)
                 this.$nextTick(() => {
                     this.$bvModal.hide('delMenu')
@@ -205,4 +204,5 @@ export default {
 .titlefont {font-size: 50px;}
 .content {font-size: 25px;}
 #sigimg {float:left}
+#menuimg {width: 240px; height:200px;}
 </style>
