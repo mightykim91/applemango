1feb274e3082938f2bd5fe2da5c21ebbc54f0cd8
박기훈 <gh9411@gmail.com>
2020-08-06 23:49:09 +0900

    impl signUp function bacl/front + Daum Adrress API import
    Former-commit-id: 59095d6f2972ed7da0d38cf76b874787c897fadc

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,7 @@ import detail from '@/views/restaurant/detail.vue';
 import comment from '@/views/admin/instagramComment.vue';
 
 import mypage from '@/views/user/myPage.vue';
+import addressmodal from '@/components/user/AddressForm.vue'
 
 Vue.use(VueRouter);
 
@@ -100,7 +101,12 @@ export const router = new VueRouter({
       name:'mypage',
       component: mypage,
       
-    }
+    },
+    {
+      path: '/addressmodal',
+      name: 'addressmodal',
+      component: addressmodal
+    },
 
     
   ]
