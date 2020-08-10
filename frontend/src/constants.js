51b0ab7578012fba98516de6d0e631e44d923668
Ubuntu <ubuntu@ip-172-26-2-165.ap-northeast-2.compute.internal>
2020-08-10 02:37:08 +0000

    springboot applied https
    Former-commit-id: 1b6c5be9448950028e4d0d1568b37ca6f0a5a8fe [formerly cf198abd01555de052d54a6349f216f35983bef3]
    Former-commit-id: d983f62f7bbbfe749d0b96717b690daf8fe70838

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,5 @@
 export default Object.freeze({
-    URL: 'http://i3a503.p.ssafy.io:8080/api/'
-    //Server
-    //map:
-    //local
-    //URL: 'http://localhost:8080/',
-    MAP:'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a367e1fe90bc271522126e07ddfc6338&libraries=services'
+    URL: 'https://i3a503.p.ssafy.io:8080/api/',
+    MAP:'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a367e1fe90bc271522126e07ddfc6338&libraries=services',
 })
 
