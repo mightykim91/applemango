7bb9905f539080e4aa3116a2a51a9dffe83c2f73
박기훈 <gh9411@gmail.com>
2020-08-12 17:39:05 +0900

    deco login&join form / update input rules
    Former-commit-id: 1ec48818538c7eb347097e5baa69a70e4da235b4 [formerly 3e4036a6afe5fc077749553ed239795ec2b2547f]
    Former-commit-id: 33aa4942738721f49bf45fc53e1ad5256018d393

--- frontend/src/views/user/login.vue
+++ frontend/src/views/user/login.vue
@@ -2,7 +2,74 @@
 
 <template>
     <div id="login">
-    <header/>
+    
+    <v-app id="inspire">
+        <v-main>
+
+            <v-row
+            align="center"
+            justify="center"
+            >
+            <v-col
+                cols="1"
+                sm="12"
+                md="6"
+            >
+                <v-card class="elevation-12">
+                <v-toolbar
+                    color="#FFCB3C"
+                    dark
+                    flat
+                >
+                    <v-toolbar-title>Login</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-btn
+                        :href="source"
+                        icon
+                        large
+                        target="_blank"
+                        v-on="on"
+                        >
+                        <v-icon>mdi-code-tags</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Source</span>
+                    </v-tooltip>
+                </v-toolbar>
+                <v-card-text>
+                    <v-form>
+                    <v-text-field
+                        label="Login"
+                        name="login"
+                        prepend-icon="mdi-account"
+                        type="text"
+                        v-model="user.id"
+                    ></v-text-field>
+
+                    <v-text-field
+                        id="password"
+                        label="Password"
+                        name="password"
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        v-model="user.password" @keyup.enter="login()"
+                    ></v-text-field>
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn yellow @click="$router.push({ name: 'SignUp' })">Register</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="#FFCB3C" v-on:click="login()">Login</v-btn>
+                </v-card-actions>
+                </v-card>
+            </v-col>
+            </v-row>
+ 
+        </v-main>
+    </v-app>
+
     <div class="login">
         <h2 class="comm__title">LOGIN</h2>
 
