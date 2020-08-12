d4b427c83002c10255f57f9c63f79323dcb27622
문명기 <myunggi_moon@naver.com>
2020-08-12 21:00:54 +0900

    make markers using restaurants
    Former-commit-id: 8ff3614eb18ddb8aa07cb2d08a6ed17e51dfa75f [formerly 95909fc03ff7d3035bb54a8390ab792fc2ae3ecb]
    Former-commit-id: 5da19a453775f8c34f76845a3327581041276d5e

--- frontend/src/components/mainView/MapComponent.vue
+++ frontend/src/components/mainView/MapComponent.vue
@@ -41,10 +41,13 @@ export default {
             nearByRestaurants: ['음식점1', '음식점2', '음식점3'],
             addr : '서울특별시 강남구 역삼동 테헤란로 212',
             uid : this.$parent.uid,
-            userInfo : []
+            userInfo : [],
+            rlist:[],
         }
     },
+
      mounted() {
+         this.getAllRestaurants();
          console.log(this.uid)
         axios.get(BACKEND_URL + 'user/info', {params: {'uid':this.uid}})
         .then(response => {
@@ -52,19 +55,17 @@ export default {
             this.addr = this.userInfo.uaddr
         })
 
-        if (window.kakao && window.kakao.maps) {
-            this.initMap();
-        } else {
-            const script = document.createElement('script');
-            /* global kakao */
-            script.onload = () => kakao.maps.load(this.initMap);
-            //script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a367e1fe90bc271522126e07ddfc6338'
-            script.src = MAP_URL;
-            document.head.appendChild(script);
-        }
     },
     methods: {
         //지도
+        getAllRestaurants(){ // 모든 레스토랑 정보를 받아온다. mount 때 실행하여 watch를 이용해 rlist가바뀌면 initmap에 적용한다.
+            axios
+            .get(BACKEND_URL+'rst/all')
+            .then(({ data }) =>{
+                this.rlist =data;
+                
+            })
+        },//end of getAllRestaurants
         initMap() {
             var container = document.getElementById('map');
             var options = {
@@ -94,32 +95,61 @@ export default {
                     map.setCenter(coords);
                 }
             });
-            geocoder.addressSearch("강남구 테헤란로4길 27", (result, status) => {
-                // 정상적으로 검색이 완료됐으면 
-                if (status == kakao.maps.services.Status.OK) {
-                    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-                    // 결과값으로 받은 위치를 마커로 표시합니다
-                    var marker = new kakao.maps.Marker({
-                        map: map,
-                        position: coords
-                    });
-                    // 인포윈도우로 장소에 대한 설명을 표시합니다
-                    var infowindow = new kakao.maps.InfoWindow({
-                        content: '<div style="width:150px;text-align:center;padding:6px 0;">홍콩반점0410</div>'
-                    });
-                    infowindow.open(map, marker);  
-                    kakao.maps.event.addListener(marker, 'click', function() {
-                        // 해당 매장으로 이동
-                        router.push({ name: 'storeDetail', params: { rid: '1' }});
-                        //<router-link :to="{ name: "storeDetail", params: { rid: 1 }}"> 홍콩반점0410<br>강남역점 </router-link>
-                    });
-                    //infowindow.open(map, marker);
-                    // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-                    map.setCenter(coords);
-                }
-            });
+           // "강남구 테헤란로4길 27",1,"홍콩반점0410"
+
+           for(let i=0; i< this.rlist.length ;i++){
+          // .forEach(element => {
+                
+                geocoder.addressSearch(this.rlist[i].raddr, (result, status) => {
+                    // 정상적으로 검색이 완료됐으면 
+                    if (status == kakao.maps.services.Status.OK) {
+                        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+                        // 결과값으로 받은 위치를 마커로 표시합니다
+                        var marker = new kakao.maps.Marker({
+                            map: map,
+                            position: coords
+                        });
+                        // 인포윈도우로 장소에 대한 설명을 표시합니다
+                        var infowindow = new kakao.maps.InfoWindow({
+                            content: '<div style="width:150px;text-align:center;padding:6px 0;">'+ this.rlist[i].rname +'</div>'
+                        });
+                        infowindow.open(map, marker);  
+                        kakao.maps.event.addListener(marker, 'click', function() {
+                            // 해당 매장으로 이동
+                            router.push({ name: 'storeDetail', params: { rid: this.rlist[i].rid }});
+                            //<router-link :to="{ name: "storeDetail", params: { rid: 1 }}"> 홍콩반점0410<br>강남역점 </router-link>
+                        });
+                        //infowindow.open(map, marker);
+                        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                        map.setCenter(coords);
+                    }
+                });// end of geocoder.addressSearch
+             // });// end of foreach
+            }// end of for
             
-        }//end of initMap
+        },//end of initMap
+        kakaoMapAPIReady(){
+            if (window.kakao && window.kakao.maps) {
+                this.initMap();
+            } else {
+                const script = document.createElement('script');
+                /* global kakao */
+                script.onload = () => kakao.maps.load(this.initMap);
+                //script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a367e1fe90bc271522126e07ddfc6338'
+                script.src = MAP_URL;
+                document.head.appendChild(script);
+            }
+        },//end of kakaoMapAPIReady
+        
+        
+      
+        
+    }, // methods
+    watch:{
+        rlist:function(){ //페이스북 로그인해서 accesstoken 이 바뀌면 
+            console.dir(this.rlist);
+            this.kakaoMapAPIReady();// Accountid를 가져오는 함수 실행 
+        },
     }
 }
 </script>
