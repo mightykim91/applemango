3ce12fa17ac0b7b5be38d61d7112f8e09785647c
So Ra Chin <sorac725@gmail.com>
2020-08-21 01:47:28 +0900

    mod Mainview
    Former-commit-id: 3cc5f8ee39605077e2f8cc88ddbed6f1f72493b3 [formerly fba852217aa3d59af0caa878015cd604ca9df3ae]
    Former-commit-id: 2db351e6a89264db0ace22a523f49eb05dd921ef

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -5,20 +5,17 @@
 
     <v-container>
       <v-row no-gutters>
-        <v-col>
-          <!-- <a href="https://i3a503.p.ssafy.io/search/result/돈까스"> -->
-            <img class = "scale" id = "image" v-on:click="search('돈까스')" src="../assets/katsu.jpg"/>
-          <!-- </a> -->
+        <v-col class = "scale" v-on:click="search('돈까스')">
+            <img id = "image" src="../assets/katsu.jpg"/>
+            <div class="text" >돈까스</div>
         </v-col>
-        <v-col>
-            <!-- <a href="https://i3a503.p.ssafy.io/search/result/파스타"> -->
-            <img class = "scale" id = "image" v-on:click="search('파스타')" src="../assets/pasta.jpg"/>
-          <!-- </a> -->
+        <v-col class = "scale" v-on:click="search('파스타')">
+            <img id = "image" src="../assets/pasta.jpg"/>
+            <div class="text">파스타</div>
         </v-col>
-        <v-col>
-          <!-- <a href="https://i3a503.p.ssafy.io/search/result/짜장면"> -->
-            <img class = "scale" id = "image" v-on:click="search('짜장면')" src="../assets/blackbean.jpg"/>
-          <!-- </a> -->
+        <v-col class = "scale" v-on:click="search('짜장면')">
+            <img id = "image" src="../assets/blackbean.png"/>
+            <div class="text">짜장면</div>
         </v-col>
       </v-row>
     </v-container>
@@ -39,40 +36,22 @@ export default {
     //     return {
     //     }
     // }
-    // methods: {
-    //     search: function(word){
-    //       console.log('https://i3a503.p.ssafy.io/'+'search/result/'+word)
-    //       this.$router.push({ name: 'SearchResult', params: { keyword: word }})
-    //       // this.$router.push('search/result/'+ word)
-    //       // location.replace('https://i3a503.p.ssafy.io/'+'search/result/'+word)
-    //     },
-    // }
+    methods: {
+        search: function(word){
+          console.log('https://i3a503.p.ssafy.io/'+'search/result/'+word)
+          this.$store.commit('search', word)
+          this.$router.push({ name: 'SearchResult', params: { keyword: word }})
+          // this.$router.push('search/result/'+ word)
+          // location.replace('https://i3a503.p.ssafy.io/'+'search/result/'+word)
+        },
+    }
 }
 </script>
 
 <style>
 
-#image {
-  cursor:pointer; 
-  width:300px;
-  height: 250px;
-  display: inline-block;
-  transition: .5s ease;
-  backface-visibility: hidden;
-}
-
-/* .container:hover .image {
-  opacity: 0.3;
-} */
-/* 
-.text {
-  background-color: #4CAF50;
-  color: white;
-  font-size: 16px;
-  padding: 16px 32px;
-} */
-
 .scale {
+  cursor:pointer;
   transform: scale(1);
   -webkit-transform: scale(1);
   -moz-transform: scale(1);
@@ -81,11 +60,40 @@ export default {
   transition: all 0.3s ease-in-out;   /* 부드러운 모션을 위해 추가*/
 }
 .scale:hover {
-  transform: scale(1.5);
-  -webkit-transform: scale(1.5);
-  -moz-transform: scale(1.5);
-  -ms-transform: scale(1.5);
-  -o-transform: scale(1.5);
-  /* opacity: 0.3; */
+  transform: scale(1.6);
+  -webkit-transform: scale(1.6);
+  -moz-transform: scale(1.6);
+  -ms-transform: scale(1.6);
+  -o-transform: scale(1.6);
 }
+
+#image { 
+  width:300px;
+  height: 250px;
+  display: inline-block;
+  transition: .5s ease;
+  backface-visibility: hidden;
+}
+
+.text {
+  opacity: 0;
+  font-weight: bold;
+  transform: translate( -50%, -50% );
+  position: absolute;
+	top: 50%;
+	left: 50%;
+  font-size: 32px;
+  color: black;
+  /* background-color: gold; */
+  /* padding: 8px 16px; */
+}
+
+.scale:hover #image {
+  opacity: 0.5;
+}
+
+.scale:hover .text {
+  opacity: 1;
+}
+
 </style>
