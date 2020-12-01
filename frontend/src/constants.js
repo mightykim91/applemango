6c7bc6741fd0893d70ae0358868abec08e2a2a4f
mightykim91 <mightykim91@naver.com>
2020-12-02 02:37:28 +0900

    [Front] Delete crediential

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,7 +2,5 @@ export default Object.freeze({
     URL: 'https://i3a503.p.ssafy.io:8443/api/',
     MAP:'https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=a367e1fe90bc271522126e07ddfc6338&libraries=services',
     // URL: 'http://localhost:8080/'
-    
-    KAKAO_MAP_API_KEY: "614067631e0f9604071eb7c0830995fa"
 })
 
